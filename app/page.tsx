88d2dd4830ce6f014a38055b1c54e261bb1527e3
typeYu <mytypeyu@gmail.com>
2026-01-23 14:20:16 +0900

    feat: add AgreeTerums #5

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,34 @@
+'use client';
+
+import { useState } from 'react';
+
+import { AgreeTerms } from '@/components/cmm/AgreeTerms';
+
 export default function Home() {
-  return <div>home</div>;
+  const [base, setBase] = useState(false);
+  const [pension, setPension] = useState(false);
+  const [fund, setFund] = useState(false);
+
+  return (
+    <AgreeTerms
+      label="전체 동의"
+      items={[
+        {
+          content: '금융거래 기본약관',
+          checked: base,
+          onCheckedChange: setBase,
+        },
+        {
+          content: '연금저축 약관',
+          checked: pension,
+          onCheckedChange: setPension,
+        },
+        {
+          content: '집합투자규약',
+          checked: fund,
+          onCheckedChange: setFund,
+        },
+      ]}
+    />
+  );
 }
