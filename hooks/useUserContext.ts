aaa9b701543f7f4f11c35fdca3b19bf71121bd73
Seon-U <seonu.kim.kr@gmail.com>
2026-01-26 15:28:01 +0900

    refactor: useUserContext.ts 에 ctx가 undefined인 경우 에러 내도록 안정화 추가 #25

--- hooks/useUserContext.ts
+++ hooks/useUserContext.ts
@@ -5,5 +5,8 @@ import { UserContext } from './UserContextProvider';
 
 export function useUserContext() {
   const ctx = useContext(UserContext);
+  if (ctx === undefined) {
+    throw new Error('useUserContext must be used within a UserContextProvider');
+  }
   return ctx;
 }
