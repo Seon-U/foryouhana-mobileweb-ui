aaac3608dd1ec5f8a786f111ac8f1e0e136a08d3
Jeongsu0880 <latsyrc900@gmail.com>
2026-01-26 17:20:40 +0900

    feat: state 값이 많아져서 useReducer로 상태 관리 훅 변경, [이 플랜으로 변경하기] 누르면 sessionStorage 바뀌도록 구현 #24

--- app/onboarding/edit/page.tsx
+++ app/onboarding/edit/page.tsx
@@ -1,11 +1,21 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { stat } from 'fs';
+import { useRouter } from 'next/navigation';
+import { useEffect, useReducer, useState } from 'react';
 import { CustomButton } from '@/components/cmm/CustomButton';
 import Header from '@/components/cmm/Header';
 import GiftSection from './GiftSection';
 import PensionSection from './PensionSection';
 
-export type DraftPlanPayload = {
+type PlanFormState = {
+  isPension: boolean;
+  isGift: boolean;
+  isRegular: boolean;
+  monthlyMoney: number;
+  inMonth: number;
+};
+
+type DraftPlanPayload = {
   updated_at: string; // ISO string
   plan: {
     goal_money: number;
@@ -17,12 +27,42 @@ export type DraftPlanPayload = {
   };
 };
 
+function buildDraftFromForm(
+  form: PlanFormState,
+  prevDraft: DraftPlanPayload,
+): DraftPlanPayload {
+  return {
+    updated_at: new Date().toISOString(),
+    plan: {
+      ...prevDraft.plan,
+
+      acc_type: form.isPension ? 'PENSION' : 'DEPOSIT',
+      monthly_money: form.monthlyMoney,
+      in_month: form.inMonth,
+
+      is_promise_fixed: form.isGift && form.isRegular,
+      in_type: form.isRegular,
+    },
+  };
+}
+
 export default function planEdit() {
   const [isReady, setIsReady] = useState(false);
   const [draft, setDraft] = useState<DraftPlanPayload | null>(null);
-  const [pensionSelected, setPensionSelected] = useState<boolean>(false);
-  const [giftSelected, setGiftSelected] = useState<boolean>(true);
-  const [isRegular, setIsRegular] = useState(true);
+  const [form, dispatch] = useReducer(
+    (state: PlanFormState, action: Partial<PlanFormState>) => ({
+      ...state,
+      ...action,
+    }),
+    {
+      isPension: false,
+      isGift: true,
+      isRegular: true,
+      monthlyMoney: 0,
+      inMonth: 0,
+    },
+  );
+  const router = useRouter();
 
   useEffect(() => {
     console.log('mounted');
@@ -35,15 +75,15 @@ export default function planEdit() {
 
       console.log(parsed.plan);
       if (parsed?.plan.acc_type === 'PENSION') {
-        setPensionSelected(true);
-      } else setPensionSelected(false);
+        dispatch({ isPension: true });
+      } else dispatch({ isPension: false });
       if (parsed?.plan.is_promise_fixed === true) {
-        setGiftSelected(true);
-        setIsRegular(true);
+        dispatch({ isGift: true });
+        dispatch({ isRegular: true });
       } else {
-        setGiftSelected(false);
-        if (parsed?.plan.in_type) setIsRegular(true);
-        else setIsRegular(false);
+        dispatch({ isGift: false });
+        if (parsed?.plan.in_type) dispatch({ isRegular: true });
+        else dispatch({ isRegular: false });
       }
     } catch {
       // 깨진 데이터면 폐기
@@ -56,6 +96,19 @@ export default function planEdit() {
     return null;
   }
 
+  if (!draft) {
+    return '플랜이 없습니다.';
+  }
+
+  const handleSaveDraft = () => {
+    if (!draft) return;
+
+    const nextDraft = buildDraftFromForm(form, draft);
+
+    sessionStorage.setItem('giftPlan', JSON.stringify(nextDraft));
+
+    router.back();
+  };
   return (
     <div>
       <Header content="플랜 직접 수정하기" />
@@ -65,28 +118,31 @@ export default function planEdit() {
         </h1>
         <div className="rounded-2xl border border-hana-gray-300 p-5 shadow-hana-gray-300 shadow-sm">
           <PensionSection
-            select={pensionSelected}
+            select={form.isPension}
             onSelect={(isSelected) => {
-              setPensionSelected(isSelected);
+              dispatch({ isPension: isSelected });
             }}
           />
           <GiftSection
-            giftPlan={giftSelected}
+            giftPlan={form.isGift}
             onGiftPlanSelected={(isSelected) => {
-              setGiftSelected(isSelected);
-              setIsRegular(isSelected);
+              dispatch({ isGift: isSelected, isRegular: isSelected });
             }}
-            regular={isRegular}
+            regular={form.isRegular}
             onRegularSelected={(isSelected) => {
-              setIsRegular(isSelected);
+              dispatch({ isRegular: isSelected });
             }}
             monthlyMoney={Number(draft?.plan.monthly_money)}
             inMonth={Number(draft?.plan.in_month)}
           />
         </div>
         <div className="grid justify-center gap-2 pt-4">
-          <CustomButton preset="lightgraylong">돌아가기</CustomButton>
-          <CustomButton preset="greenlong">이 플랜으로 변경하기</CustomButton>
+          <CustomButton preset="lightgraylong" onClick={router.back}>
+            돌아가기
+          </CustomButton>
+          <CustomButton preset="greenlong" onClick={handleSaveDraft}>
+            이 플랜으로 변경하기
+          </CustomButton>
         </div>
       </div>
     </div>
