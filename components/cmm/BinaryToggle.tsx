e910544f7b88ec4d22d23fa9ab6138703d3244a6
Jeongsu0880 <latsyrc900@gmail.com>
2026-01-26 19:48:12 +0900

    style: onboarding/edit page에 주석 추가 #24

--- components/cmm/BinaryToggle.tsx
+++ components/cmm/BinaryToggle.tsx
@@ -1,5 +1,13 @@
 import { CustomButton } from '@/components/cmm/CustomButton';
 
+/**
+ * @page: BinaryToggle
+ * @description: 플랜 수정 페이지에 사용되는 두가지 버튼입니다.
+ * onChange 이벤트 리스너로 버튼선택 여부에 따라 토글 상태를 변경할 수 있습니다.
+ * @author: 이정수
+ * @date: 2026-01-26
+ */
+
 type BinaryToggleProps = {
   value: boolean;
   onChange: (v: boolean) => void;
