630d8ba3b63b9ce60e22369505e0748cfd0f05a2
zerouriban <minyoung6048@gmail.com>
2026-01-27 19:41:42 +0900

    style: 배경 이미지 추가 및 이미지 리소스 추가 #40

--- app/main/[childId]/beforeJoin/hometax-report/page.tsx
+++ app/main/[childId]/beforeJoin/hometax-report/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import { useParams, useRouter } from 'next/navigation';
 import { useState } from 'react';
+
 import { CustomButton } from '@/components/cmm/CustomButton';
 import Header from '@/components/cmm/Header';
 import { HometaxReportCard } from '@/components/cmm/HometaxSupportCard';
+import { IMAGES_PATH } from '@/constants/images';
 
 /**
  * @page: HomeTaxReportPage
@@ -22,40 +25,61 @@ export default function HomeTaxReportPage() {
   const [isReported, setIsReported] = useState(false);
 
   const router = useRouter();
+  const params = useParams();
 
   const handleNext = () => {
-    router.push('/main/[childId]/home');
+    router.push(`/main/${params.childId}/home`);
   };
 
   return (
     <div className="flex flex-col">
+      <div className="relative min-h-full">
+        <Image
+          src={IMAGES_PATH.HOMETAX_TREEIMG}
+          alt="background"
+          width={600}
+          height={800}
+          className="pointer-events-none absolute top-[360px] left-[-100px] opacity-20"
+          priority
+        />
+      </div>
       <Header content="홈택스 신고" />
       <section className="mx-3 mt-5">
-        <h1 className="font-hana-cm text-[16px] leading-tight">
+        <h1 className="font-hana-cm text-[22px] leading-tight">
           유기정기금 신고를 진행해주세요.
         </h1>
       </section>
+      <div className="relative z-10 mt-30">
+        <section className="mx-3 mt-3">
+          <HometaxReportCard onReportComplete={() => setIsReported(true)} />
+        </section>
 
-      <section className="mx-3 mt-3">
-        <HometaxReportCard onReportComplete={() => setIsReported(true)} />
-      </section>
+        <section className="mx-4 mt-3.5 space-y-1">
+          <p className="font-hana-regular text-[13px] text-hana-red">
+            ※ 다음 안내사항을 반드시 숙지해주세요.
+          </p>
+          <p className="font-hana-medium text-[12px] text-hana-gray-600 leading-relaxed">
+            유기정기금 중단·변경 사항은 본 서비스에 자동 반영되지 않습니다.
+            <br />
+            국세청 신고 및 서비스 내 플랜 변경은 사용자가 직접 진행해야 합니다.
+            서비스 표시는 증여 관리 참고용입니다.
+          </p>
+        </section>
+      </div>
 
-      <section className="mx-4 mt-5 space-y-1">
-        <p className="font-hana-regular text-[9px] text-hana-red">
-          ※ 다음 안내사항을 반드시 숙지해주세요.
-        </p>
-        <p className="font-hana-medium text-[10px] text-hana-gray-600 leading-relaxed">
-          유기정기금 중단·변경 사항은 본 서비스에 자동 반영되지 않습니다.
-          <br />
-          국세청 신고 및 서비스 내 플랜 변경은 사용자가 직접 진행해야 합니다.
-          <br />
-          서비스 표시는 증여 관리 참고용입니다.
-        </p>
-      </section>
+      <div className="flex w-full justify-end">
+        <Image
+          src={IMAGES_PATH.HOMETAX_STARTSPRING}
+          alt={'starspringseed'}
+          width={150}
+          height={150}
+          className="mt-15"
+        />
+      </div>
 
-      <div className="px-3 pt-10">
+      <div className="-mt-55 relative z-10 px-3">
         <CustomButton
-          className="mt-85"
+          className="mt-55"
           preset="greenlong"
           disabled={!isReported}
           onClick={handleNext}
--- constants/images.ts
+++ constants/images.ts
@@ -1,5 +1,7 @@
-export const TIMELINE_IMAGES = {
+export const IMAGES_PATH = {
   STAR_BG: '/timeline/star_bg.png',
   STAR_ICON: '/timeline/icon/star.png',
   POPUP_ILLUSTRATION: '/timeline/timeline_popupImg.png',
+  HOMETAX_STARTSPRING: '/hometax/starSpringSeed.svg',
+  HOMETAX_TREEIMG: '/hometax/tree.svg',
 } as const;
