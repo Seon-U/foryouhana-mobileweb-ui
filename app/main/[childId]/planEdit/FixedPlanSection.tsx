ca35382a5cb1145a8e5e315dcc54b3561dec78d8
Jeongsu0880 <latsyrc900@gmail.com>
2026-01-28 09:00:08 +0900

    fix: 유기정기금 신청 버튼으로 플랜 변경하기 버튼 활성화 #27

--- app/main/[childId]/planEdit/FixedPlanSection.tsx
+++ app/main/[childId]/planEdit/FixedPlanSection.tsx
@@ -1,18 +1,84 @@
 'use client';
-import { useState } from 'react';
 import { CustomButton } from '@/components/cmm/CustomButton';
+import { HometaxReportCard } from '@/components/cmm/HometaxSupportCard';
 import TitlePlanSelect from '@/components/cmm/TitlePlanSelect';
 
-export default function FixedPlanSection({ prev }: { prev: boolean }) {
-  const [isFixedGift, _setIsFixedGift] = useState<boolean>(prev);
-
+export default function FixedPlanSection({
+  isFixed,
+  prev,
+  onChange,
+  onBlockedChange,
+}: {
+  isFixed: boolean;
+  onChange: (v: boolean) => void;
+  prev: boolean;
+  onBlockedChange: (v: boolean) => void;
+}) {
   return (
     <div>
       <TitlePlanSelect title="유기정기금" />
-      {isFixedGift === true ? (
-        <CustomButton preset="redlong">중단 및 변경하기</CustomButton>
+      {prev ? (
+        <div>
+          {isFixed ? (
+            <CustomButton
+              preset="redlong"
+              onClick={() => {
+                onChange(!isFixed);
+                onBlockedChange(true);
+              }}
+            >
+              중단 및 변경하기
+            </CustomButton>
+          ) : (
+            <div>
+              <CustomButton
+                preset="greenlong"
+                onClick={() => {
+                  onChange(!isFixed);
+                  onBlockedChange(false);
+                }}
+              >
+                변경 취소하기
+              </CustomButton>
+              <div className="flex items-center justify-center pt-4">
+                <HometaxReportCard
+                  onReportComplete={() => onBlockedChange(false)}
+                />
+              </div>
+            </div>
+          )}
+        </div>
       ) : (
-        <CustomButton preset="greenlong">변경 취소하기</CustomButton>
+        <div>
+          {!isFixed ? (
+            <CustomButton
+              preset="lightgreenlong"
+              onClick={() => {
+                onChange(!isFixed);
+                onBlockedChange(true);
+              }}
+            >
+              신고하기
+            </CustomButton>
+          ) : (
+            <CustomButton
+              preset="lightgreenlong"
+              onClick={() => {
+                onChange(!isFixed);
+                onBlockedChange(false);
+              }}
+            >
+              신고하지 않기
+            </CustomButton>
+          )}
+          {isFixed && (
+            <div className="flex items-center justify-center pt-4">
+              <HometaxReportCard
+                onReportComplete={() => onBlockedChange(false)}
+              />
+            </div>
+          )}
+        </div>
       )}
     </div>
   );
--- app/main/[childId]/planEdit/GiftPlanSection.tsx
+++ app/main/[childId]/planEdit/GiftPlanSection.tsx
@@ -1,7 +1,9 @@
 'use client';
 import { useState } from 'react';
 import { CustomButton } from '@/components/cmm/CustomButton';
+import { InputMonth } from '@/components/cmm/InputDayAmount';
 import TitlePlanSelect from '@/components/cmm/TitlePlanSelect';
+import FixedPlanSection from './FixedPlanSection';
 import MethodSection from './MethodSection';
 
 export enum GIFT_METHOD {
@@ -14,25 +16,67 @@ export default function GiftPlanSection({
   period,
   amount,
   isFixed,
+  onBlockedChange,
+  isBlocked,
 }: {
   method: GIFT_METHOD;
   period: number | null;
   amount: number | null;
   isFixed: boolean;
+  isBlocked: boolean;
+  onBlockedChange: (b: boolean) => void;
 }) {
-  const [giftMethod, setGiftMethod] = useState<GIFT_METHOD>(method);
+  const [giftMethod, setGiftMethod] = useState<GIFT_METHOD>(
+    isFixed ? GIFT_METHOD.REGULAR : method,
+  );
   const [newAmount, setNewAmount] = useState<number | null>(amount);
   const [newPeriod, setNewPeriod] = useState<number | null>(period);
+  const [fixed, setFixed] = useState<boolean>(isFixed);
   return (
     <div>
       <TitlePlanSelect title="증여 방식" />
-      {isFixed === true ? (
-        <CustomButton disabled preset="lightgraylong" className="grid gap-0">
-          <span className="text-[14px] text-hana-black">정기 이체 방식</span>
-          <span className="text-[9px] text-hana-black">유기정기금 운용 중</span>
-        </CustomButton>
+      {isFixed ? (
+        <div>
+          <div>
+            <CustomButton
+              disabled
+              preset="lightgraylong"
+              className="grid gap-0"
+            >
+              <span className="text-[14px] text-hana-black">
+                정기 이체 방식
+              </span>
+              <span className="text-[9px] text-hana-black">
+                유기정기금 운용 중
+              </span>
+            </CustomButton>
+            <div className="flex justify-between pt-4">
+              <div>
+                <TitlePlanSelect title="증여 기간" />
+                <InputMonth
+                  disabled={true}
+                  value={period ?? undefined}
+                  unit="개월"
+                  className="'h-[42px] w-[155px] bg-hana-gray-300 font-hana-regular text-hana-gray-600'"
+                  onChange={(v) => setNewPeriod(v ?? null)}
+                />
+              </div>
+              <div>
+                <TitlePlanSelect title="월 증여액" />
+                <InputMonth
+                  disabled={true}
+                  value={amount ?? undefined}
+                  unit="원"
+                  className="'h-[42px] w-[155px] bg-hana-gray-300 font-hana-regular text-hana-gray-600'"
+                  onChange={(v) => setNewAmount(v ?? null)}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       ) : (
         <MethodSection
+          isBlocked={isBlocked}
           isRegular={giftMethod === GIFT_METHOD.REGULAR}
           onChange={setGiftMethod}
           isFixed={isFixed}
@@ -42,6 +86,16 @@ export default function GiftPlanSection({
           onChangePeriod={setNewPeriod}
         />
       )}
+      <hr className="my-4 border-hana-gray-400" />
+      <FixedPlanSection
+        prev={isFixed}
+        isFixed={fixed}
+        onChange={(v: boolean) => {
+          setFixed(v);
+          if (v) setGiftMethod(GIFT_METHOD.REGULAR);
+        }}
+        onBlockedChange={onBlockedChange}
+      />
     </div>
   );
 }
